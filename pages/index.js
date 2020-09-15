8fb49aee89523170af41b05f0fde7d1e13a08eb5
unknown <chris.a.desilva@gmail.com>
2020-09-15 09:07:47 -0500

    Style homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Head from "next/head";
+import Link from "next/link";
 
-export default function Home({ park }) {
-  const acadia = park.data[0];
+export default function Home({ parks }) {
+  console.log(parks);
+  const [parkIndex, setParkIndex] = useState(0);
+  const [selected, setSelected] = useState({
+    image: parks.data[parkIndex].images[0].url,
+    title: parks.data[parkIndex].fullName,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleChangePark = (e) => {
+    setParkIndex(e.target.value);
+  };
+
+  useEffect(() => {
+    setSelected({
+      image: parks.data[parkIndex].images[0].url,
+      title: parks.data[parkIndex].fullName,
+    });
+  }, [parkIndex]);
+
+  useEffect(() => {
+    if (parks.data) {
+      setIsLoading(false);
+    }
+  }, [parks]);
+
+  if (isLoading) return <p>Loading...</p>;
 
   return (
     <div>
@@ -12,9 +39,55 @@ export default function Home({ park }) {
       </Head>
 
       <main>
-        <header className="flex flex-col items-center justify-center h-screen">
-          <h1 className="text-red-700">My Next Park</h1>
-          <h2>{acadia.fullName}</h2>
+        <nav className="border-b-2 border-green-700 fixed text-center w-full py-4 bg-white">
+          <Link href="/">
+            <a className="text-green-700 hover:text-green-900 transition-colors duration-300">
+              My Next Park
+            </a>
+          </Link>
+        </nav>
+        <header className="flex flex-col items-center justify-center container py-24 h-screen">
+          <h1 className="mt-32 mb-12">Find Your Next Adventure</h1>
+          <div className="w-full flex">
+            <div className="flex flex-col items-center w-1/2">
+              <div className="border-2 border-gray p-8 shadow">
+                <div className="h-64 w-64 overflow-y-hidden">
+                  <img
+                    className="rounded"
+                    src={selected.image}
+                    alt={selected.title}
+                  />
+                </div>
+                <label>Choose a park</label>
+                <select
+                  onChange={handleChangePark}
+                  className="block w-64 mx-auto bg-white border-2 border-gray-700 text-gray-700 py-3 px-4 focus:border-gray-500"
+                >
+                  {parks.data.map((park, idx) => {
+                    if (park.designation === "National Park") {
+                      return (
+                        <option key={park.id} value={idx}>
+                          {park.fullName}
+                        </option>
+                      );
+                    }
+                  })}
+                </select>
+                <Link
+                  href={`/parks/${selected.title
+                    .replaceAll(" ", "-")
+                    .toLowerCase()}`}
+                >
+                  <a className="w-24 block mx-auto mt-4 border-2 py-2 border-green-700 bg-green-700 hover:bg-white text-white hover:text-green-700 transition-colors duration-100 text-center">
+                    Let's Go!
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div className="w-1/2 flex items-center justify-center">
+              <img className="w-full" src="/navigator.svg" />
+            </div>
+          </div>
         </header>
       </main>
     </div>
@@ -23,13 +96,13 @@ export default function Home({ park }) {
 
 export const getStaticProps = async () => {
   const res = await axios.get(
-    `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${process.env.PARKS_API_KEY}`
+    `https://developer.nps.gov/api/v1/parks?limit=500&api_key=${process.env.PARKS_API_KEY}`
   );
-  const park = res.data;
+  const parks = res.data;
 
   return {
     props: {
-      park,
+      parks,
     },
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,24 @@ module.exports = {
   },
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    boxShadow: {
+      xs: "0 0 0 1px rgba(0, 0, 0, 0.05)",
+      sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+      default:
+        "1px 1px #4a5568, 2px 2px #4a5568, 3px 3px #4a5568, 4px 4px #4a5568",
+      md:
+        "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+      lg:
+        "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      xl:
+        "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+      "3xl": "0 35px 60px -15px rgba(0, 0, 0, 0.3)",
+      inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+      outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+      focus: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+      none: "none",
+    },
     extend: {
       colors: {
         "accent-1": "#333",
